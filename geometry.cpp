ecbfb5ab14a6ac96d5c218f417477657d7962ae0
Naumov Nikita <nikita.naumov.02@gmail>
2021-02-19 20:41:15 +0700

    input objects

---
+++ geometry.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+    vector <string> list;
+    string s;
+    int i;
+    cout << "¬ведите фигуры: " << endl;
+    while (getline(cin, s)) {
+        if (s == "") {
+            break;
+        }
+        list.push_back(s);
+    }
+}
