217098ef5ad814c6868119b287963b0e64cb5d5c
Naumov Nikita <nikita.naumov.02@gmail>
2021-02-25 14:08:08 +0700

    new void error

--- geometry.cpp
+++ geometry.cpp
@@ -1,79 +1,45 @@
 #include <iostream>
-#include <cstdlib>
+#include <map>
 #include <string>
 #include <vector>
-#include <cmath>
 
 using namespace std;
-const int T = 8; // 8 параметров у треугольника
 
-void check(string& s)
+void error(int code, int position)
 {
-    string tr = "triangle(", cir = "circle(";
-    for (int j = 0; j < size(s); j++) 
-    {
-        s[j] = tolower(s[j]); //приводим к строчному регистру
+    for (int i = 0; i < position; i++) {
+        cout << " ";
     }
-    if (s.find(tr) == 0 || s.find(cir) == 0)  // если введено верно
-    { 
+    cout << "^" << endl;
 
-    } 
-    else 
-    {
-        cout << "error" << endl;
+    switch (code) {
+    case 1:
+        cout << "Error at column " << position
+             << "expected 'circle' or 'triangle'; code = " << code << endl;
+        break;
+    case 2:
+        cout << "Error at column " << position
+             << ": expected '<double>'; code = " << code << endl;
+        break;
+    case 3:
+        cout << "Error at column " << position
+             << ": expected ')'; code = " << code << endl;
+        break;
+    case 4:
+        cout << "Error at column " << position
+             << ": unexpected token; code = " << code << endl;
+        break;
+    case 5:
+        cout << "Error at column " << position
+             << ": expected '('; code = " << code << endl;
+        break;
     }
 }
 
-void triangle(string& s)
-{
-    double mas[T]; // массив с параметрами треугольника
-    int k = 0; // индекс для заполнения массива с параметрами
-    string t = "";
-    if (s.find("((") < s.find("))")) // если порядок скобок соблюдён верно
-    {
-        for (int i = s.find("((") + 2; i < s.find("))"); i++) // начинаем считывать после первый символ после "((", поэтому +2
-        {
-            if (s[i] == 32 || s[i] == 44 || s[i] == 41) // если это пробел || запятая || скобка, значит перед этим было число
-            {
-                mas[k] = stod(t);
-                k++;
-                if (k == T) // если это последний элемент массива
-                {
-                    // выводим массив с параметрами и выходим из процедуры
-                    for (int i = 0; i < T; i++)
-                        cout << mas[i] << " ";
-                    cout << endl;
-                    return;
-                }
-            }
-            if ((s[i] < 47 || s[i]>58) && (s[i] != 45 && s[i] != 46)) // если это не цифра, минус или точка
-            {
-                cout << "error" << endl;
-                return;
-            }
-            t += s[i];
-        }
-    }
-}
 
 int main()
 {
-    setlocale(LC_CTYPE, "RUSSIAN");
-    vector <string> list;
-    string s;
-    int i;
-    cout << "Введите фигуры: " << endl;
-    while (getline(cin, s)) 
-    {
-        if (s == "") 
-        {
-            break;
-        }
-        list.push_back(s);
-    }
-    check(list[0]); // проверяем правильность входных данных
-    for (i = 0; i < list.size(); i++) 
-    {
-        cout << list[i] << endl;
-    }
+    setlocale(LC_CTYPE, "Russian");
+ 
+    cout << "Задайте фигуры" << endl;
 }
