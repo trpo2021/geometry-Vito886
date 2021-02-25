78f2b106d26a00789541b8fcc2a5fdc1789709d4
Naumov Nikita <nikita.naumov.02@gmail>
2021-02-25 14:11:33 +0700

    new function triangle coords

--- geometry.cpp
+++ geometry.cpp
@@ -125,6 +125,83 @@ vector<float> circleCoords(string& str)
     return ccoords;
 }
 
+vector<float> triangleCoords(string& str)
+{
+    string s = str, elem;
+    vector<float> tcoords;
+    int start = s.find("(("), end = s.find("))"), count = 0, pos = 10,
+        elemLenght = 0;
+
+    if (end == -1) {
+        error(3, s.length() - 1);
+        tcoords.clear();
+        return tcoords;
+    }
+
+    string tempMas = "";
+    tempMas.append(s, start);
+
+    if ((tempMas[0] == '(') && (tempMas[1] == '(')) {
+        tempMas.erase(0, 2);
+    } else {
+        error(5, 8);
+        tcoords.clear();
+        return tcoords;
+    }
+    int length = tempMas.length();
+    for (int i = 0; i < length; i++) {
+        elem = "";
+        if (count < 7) {
+            if (((tempMas[i] < 48) || (tempMas[i] > 57)) && (tempMas[i] != 32)
+                && (tempMas[i] != 44) && (tempMas[i] != 45)
+                && (tempMas[i] != 46)) {
+                error(2, pos);
+                tcoords.clear();
+                return tcoords;
+            }
+            if (tempMas[i] == ' ') {
+                elem.append(tempMas, 0, i);
+                tcoords.push_back(stof(elem));
+                tempMas.erase(0, i + 1);
+                length = tempMas.length();
+                i = 0;
+                count++;
+                pos++;
+            }
+            if (tempMas[i] == ',') {
+                elem.append(tempMas, 0, i);
+                tcoords.push_back(stof(elem));
+                tempMas.erase(0, i + 2);
+                length = tempMas.length();
+                i = 0;
+                count++;
+                pos += 2;
+            }
+        } else if (count == 7) {
+            int end = tempMas.find("))");
+            if (end != -1) {
+                elem.append(tempMas, 0, end);
+                tcoords.push_back(stof(elem));
+                tempMas.erase(0, end + 2);
+                length = tempMas.length();
+                i = 0;
+                count += 2;
+            } else {
+                error(3, pos);
+                tcoords.clear();
+                return tcoords;
+            }
+        } else {
+            error(4, str.find("))") + 2);
+            tcoords.clear();
+            return tcoords;
+        }
+        pos++;
+    }
+
+    return tcoords;
+}
+
 int main()
 {
     setlocale(LC_CTYPE, "Russian");
