7b6e6bf568ee7c68e1d3316b2bd0896721b1cdf7
Naumov Nikita <nikita.naumov.02@gmail>
2021-04-29 23:05:27 +0700

    first test

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,19 @@
+#define CATCH_CONFIG_MAIN
 #include <iostream>
 #include "../thirdparty/catch.hpp"
 #include <libgeometry/checks.h>
-int main(){
-    std::cout << "ff";
+#include <libgeometry/counting.h>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
+
+TEST_CASE("first test", "")
+{
+    SECTION("first section")
+    {
+        CHECK(sum(1,2) == 3);
+    }
     system("PAUSE");
-    return 0;
 }
